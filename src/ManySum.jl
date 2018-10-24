f53fdb36c13fe54e2f09fb4e481de15e8b542302
Pierre Abbat <phma@bezitopo.org>
2018-10-24 23:58:12 +0000

    Write function pairwiseSum, which appears to be working

--- src/ManySum.jl
+++ src/ManySum.jl
@@ -1,4 +1,21 @@
 module ManySum
+export pairwiseSum!
+
+function pairwiseSum!(a::Array{T})::T where T<:AbstractFloat
+  i=1
+  n=length(a)
+  while i<n
+    for j=1:2*i:n-i
+      a[j]+=a[j+i]
+    end
+    i*=2
+  end
+  if n>0
+    a[1]
+  else
+    0
+  end
+end
 
 greet() = print("Hello World!")
 
